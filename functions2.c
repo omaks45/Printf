b17d5cb92f3e2ebb5caf313d8e16876490417c05
omaks45 <danielgozie5@gmail.com>
2022-08-10 11:26:35 -0700

    top files1

--- functions2.c
+++ functions2.c
@@ -91,3 +91,34 @@ buffer[i + offset] = '\0';
 
 return (write(1, buffer, i + offset));
 }
+/**
+ *
+ *  * print_reverse - Prints reverse string.
+ *
+ *   * @types: Lista of arguments
+ *
+ *    * @buffer: Buffer array to handle print
+ *
+ *     * @flags:  Calculates active flags
+ *
+ *      * @width: get width
+ *
+ *       * @precision: Precision specification
+ *
+ *        * @size: Size specifier
+ *
+ *         * Return: Numbers of chars printed
+ *
+ *          */
+
+
+
+int print_reverse(va_list types, char buffer[],
+
+			int flags, int width, int precision, int size)
+
+{
+
+		char *str;
+
+			int i, count = 0;
