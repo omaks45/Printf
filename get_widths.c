9cadd4d50245e43a2be5eb7509ec53644395ae5f
omaks45 <danielgozie5@gmail.com>
2022-08-10 11:59:16 -0700

    top files2

--- get_widths.c
+++ get_widths.c
@@ -23,3 +23,44 @@ width += format[curr_i] - '0';
 else if (format[curr_i] == '*')
 {
 curr_i++;
+for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+
+		{
+
+					if (is_digit(format[curr_i]))
+
+								{
+
+												width *= 10;
+
+															width += format[curr_i] - '0';
+
+																	}
+
+							else if (format[curr_i] == '*')
+
+										{
+
+														curr_i++;
+
+																	width = va_arg(list, int);
+
+																				break;
+
+																						}
+
+									else
+
+													break;
+
+										}
+
+
+
+	*i = curr_i - 1;
+
+
+
+		return (width);
+
+}
--- handle_prints.c
+++ handle_prints.c
@@ -26,3 +26,47 @@ fmt_t fmt_types[] = {
 for (i = 0; fmt_types[i].fmt != '\0'; i++)
 if (fmt[*ind] == fmt_types[i].fmt)
 return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+
+
+	if (fmt_types[i].fmt == '\0')
+
+		{
+
+					if (fmt[*ind] == '\0')
+
+									return (-1);
+
+							unknow_len += write(1, "%%", 1);
+
+									if (fmt[*ind - 1] == ' ')
+
+													unknow_len += write(1, " ", 1);
+
+											else if (width)
+
+														{
+
+																		--(*ind);
+
+																					while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+
+																										--(*ind);
+
+																								if (fmt[*ind] == ' ')
+
+																													--(*ind);
+
+																											return (1);
+
+																													}
+
+													unknow_len += write(1, &fmt[*ind], 1);
+
+															return (unknow_len);
+
+																}
+
+	return (printed_chars);
+
+	}
