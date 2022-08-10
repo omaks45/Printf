d0e0c26a50f4e529afed1ebce653cc269835ae3d
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-08-10 20:10:09 +0100

    utils.c

--- utils.c
+++ utils.c
@@ -8,10 +8,10 @@
  */
 int is_printable(char c)
 {
-if (c >= 32 && c < 127)
-return (1);
+	if (c >= 32 && c < 127)
+		return (1);
 
-return (0);
+	return (0);
 }
 
 /**
@@ -23,18 +23,18 @@ return (0);
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-char map_to[] = "0123456789ABCDEF";
-/* The hexa format code is always 2 digits long */
-if (ascii_code < 0)
-ascii_code *= -1;
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
 
-buffer[i++] = '\\';
-buffer[i++] = 'x';
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
-buffer[i++] = map_to[ascii_code / 16];
-buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
 
-return (3);
+	return (3);
 }
 
 /**
@@ -45,8 +45,42 @@ return (3);
  */
 int is_digit(char c)
 {
-if (c >= '0' && c <= '9')
-return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
 
-return (0);
+	return (0);
+}
+
+/**
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
 }
